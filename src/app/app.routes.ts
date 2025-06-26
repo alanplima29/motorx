b79c18f0926fce913d1e01af5eb0c7c393363d45
alanplima29 <alanlima@alunos.utfpr.edu.br>
2025-06-25 23:06:03 -0300

    atualização com requisito 16 cumprido

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,28 @@
-import { RegistroComponent } from './pages/registro/registro.component';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-import { CadastroPecasComponent} from './pages/cadastro/cadastro-pecas.component';
-import { DetalhesPedidoComponent } from './pages//detalhes-pedido/detalhes-pedido.component';
-
-
+import { CadastroPecasComponent } from './pages/cadastro/cadastro-pecas.component';
+import { DetalhesPedidoComponent } from './pages/detalhes-pedido/detalhes-pedido.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { PedidoComponent } from './pages/pedido/pedido.component'; // ← novo import
 
 export const routes: Routes = [
-    {path: '',component: LoginComponent,},
-    { path: 'login', component: LoginComponent }, 
-    { path: 'pecas', component: CadastroPecasComponent},
-    { path: 'pedido/detalhes', component: DetalhesPedidoComponent },
-    { path: 'registro', component: RegistroComponent},
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'pecas', component: CadastroPecasComponent },
+//Req. 16
+  {
+  path: 'pedido',
+  loadComponent: () =>
+    import('./pages/pedido/pedido.component').then(m => m.PedidoComponent),
+  children: [
+    {
+      path: 'detalhes',
+      loadComponent: () =>
+        import('./pages/detalhes-pedido/detalhes-pedido.component').then(m => m.DetalhesPedidoComponent),
+    }
+  ]
+},
+
+
+  { path: 'registro', component: RegistroComponent },
 ];
---
+++ src/app/pages/pedido/pedido.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-pedido',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './pedido.component.html',
+  styleUrls: ['./pedido.component.css'],
+})
+export class PedidoComponent {}
