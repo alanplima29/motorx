3dc3a89903a12e84468f001cdccbdffed060af02
alanplima29 <alanlima@alunos.utfpr.edu.br>
2025-06-29 14:20:42 -0300

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -20,8 +20,8 @@ export default {
   ]
 },
   assets: {
-    'index.csr.html': {size: 2251, hash: '919eeeda8ad769f882d3299ca3940bba9729c0371ed46070e48aa5b7e2508064', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1064, hash: 'bbb75fbf02205420b68a0968d859dbd015751271e58fc703005b74389e182cd1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2251, hash: '4d36420a0d114e7f4ec81aaec2ff15a242de38a1205629f68a6fec680f3b9f05', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1064, hash: '93e322a14af69ac5cfe3b65bba1659d3c7255fb61ea55ca27bc1d8a05c045c97', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-ZVHHQQWS.css': {size: 20172, hash: 'eONbptb7vyc', text: () => import('./assets-chunks/styles-ZVHHQQWS_css.mjs').then(m => m.default)}
   },
 };
